cc180153dbc7135c2717ac94a679d8f9ecbb1946
Tony Barbour <tony@LunarG.com>
2015-08-06 12:41:50 -0600

    tests: Use new env variable for layers location

--- tests/run_all_tests_with_layers.ps1
+++ tests/run_all_tests_with_layers.ps1
@@ -12,7 +12,7 @@ Set-Item -path env:Path -value ($env:Path + ";..\loader\$dPath")
 Set-Item -path env:Path -value ($env:Path + ";gtest-1.7.0\$dPath")
 $env:VK_INSTANCE_LAYERS = "Threading;MemTracker;ObjectTracker;DrawState;ParamChecker;ShaderChecker"
 $env:VK_DEVICE_LAYERS = "Threading;MemTracker;ObjectTracker;DrawState;ParamChecker;ShaderChecker"
-$env:VK_LAYER_DIRS = "..\layers\$dPath"
+$env:VK_LAYER_PATH = "..\layers\$dPath"
 
 if (!(Test-Path ./vk_layer_settings.txt)) {
     Write-Host "Missing vk_layer_settings.txt file" -foregroundcolor red
