d1718641a097fea0c7e4025d2f511255dbd0cadf
Courtney Goeltzenleuchter <courtneygo@google.com>
2016-05-05 13:11:14 -0600

    build script for layers
    Depends on build bot supporting NDK
    in /buildbod/android-ndk that supports Vulkan (android-23).
    
    Change-Id: I437d64f9fa00293fb4d8891eab59e6703db9357c

--- build-android/build.py
+++ build-android/build.py
@@ -15,15 +15,143 @@
 # limitations under the License.
 #
 
+import argparse
+import multiprocessing
 import os
 import subprocess
 import sys
 
+THIS_DIR = os.path.realpath(os.path.dirname(__file__))
+
+ALL_ARCHITECTURES = (
+  'arm',
+  'arm64',
+  'mips',
+  'mips64',
+  'x86',
+  'x86_64',
+)
+
+ALL_ABIS = (
+  'armeabi',
+  'armeabi-v7a',
+  'arm64-v8a',
+  'mips',
+  'mips64',
+  'x86',
+  'x86_64',
+)
+
+def jobs_arg():
+  return '-j{}'.format(multiprocessing.cpu_count() * 2)
+
+def arch_to_abis(arch):
+  return {
+    'arm': ['armeabi', 'armeabi-v7a'],
+    'arm64': ['arm64-v8a'],
+    'mips': ['mips'],
+    'mips64': ['mips64'],
+    'x86': ['x86'],
+    'x86_64': ['x86_64'],
+  }[arch]
+
+class ArgParser(argparse.ArgumentParser):
+  def __init__(self):
+    super(ArgParser, self).__init__()
+
+    self.add_argument(
+      '--out-dir', help='Directory to place temporary build files.',
+      type=os.path.realpath, default=os.path.join(THIS_DIR, 'out'))
+
+    self.add_argument(
+      '--arch', choices=ALL_ARCHITECTURES,
+      help='Architectures to build. Builds all if not present.')
+
+    # The default for --dist-dir has to be handled after parsing all
+    # arguments because the default is derived from --out-dir. This is
+    # handled in run().
+    self.add_argument(
+      '--dist-dir', help='Directory to place the packaged artifact.',
+      type=os.path.realpath)
+
+
 def main():
-    print('Constructing Vulkan validation layer source...')
-    print('TBD...')
-    print('Done.')
+  print('Constructing Vulkan validation layer source...')
+
+  print('THIS_DIR: %s' % THIS_DIR)
+  parser = ArgParser()
+  args = parser.parse_args()
+
+  arches = ALL_ARCHITECTURES
+  if args.arch is not None:
+    arches = [args.arch]
+
+  abis = []
+  for arch in arches:
+    abis.extend(arch_to_abis(arch))
+
+  # According to vk_platform.h, armeabi is not supported for Vulkan
+  # so remove it from the abis list.
+  abis.remove('armeabi')
+
+  build_cmd = [
+    'bash', THIS_DIR + '/android-generate.sh'
+  ]
+  print('Generating generated layers...')
+  subprocess.check_call(build_cmd)
+  print('Generation finished')
+
+  if os.path.isdir('/buildbot/android-ndk'):
+    ndk_dir = '/buildbot/android-ndk'
+  elif os.path.isdir(os.environ['NDK_PATH']):
+    ndk_dir = os.environ['NDK_PATH'];
+  else:
+    print('Error: No NDK environment found')
+    return
+
+  ndk_build = os.path.join(ndk_dir, 'ndk-build')
+  platforms_root = os.path.join(ndk_dir, 'platforms')
+  toolchains_root = os.path.join(ndk_dir, 'toolchains')
+  build_dir = THIS_DIR
+
+  print('ndk_dir: %s' % ndk_dir)
+  print('ndk_build: %s' % ndk_build)
+
+  compiler = 'clang'
+  stl = 'gnustl_static'
+  obj_out = os.path.join(THIS_DIR, stl, 'obj')
+  lib_out = os.path.join(THIS_DIR, 'jniLibs')
+
+  build_cmd = [
+    'bash', ndk_build, '-C', build_dir, jobs_arg(),
+    'APP_ABI=' + ' '.join(abis),
+    # Use the prebuilt platforms and toolchains.
+    'NDK_PLATFORMS_ROOT=' + platforms_root,
+    'NDK_TOOLCHAINS_ROOT=' + toolchains_root,
+    'GNUSTL_PREFIX=',
+
+    # Tell ndk-build where all of our makefiles are and where outputs
+    # should go. The defaults in ndk-build are only valid if we have a
+    # typical ndk-build layout with a jni/{Android,Application}.mk.
+    'NDK_PROJECT_PATH=null',
+    'NDK_TOOLCHAIN_VERSION=' + compiler,
+    'APP_BUILD_SCRIPT=' + os.path.join(build_dir, 'jni', 'Android.mk'),
+    'APP_STL=' + stl,
+    'NDK_APPLICATION_MK=' + os.path.join(build_dir, 'jni', 'Application.mk'),
+    'NDK_OUT=' + obj_out,
+    'NDK_LIBS_OUT=' + lib_out,
+    'THIRD_PARTY_PATH=',
+
+    # Put armeabi-v7a-hard in its own directory.
+    '_NDK_TESTING_ALL_=yes'
+  ]
+
+  print('Building Vulkan validation layers for ABIs:' +
+    ' {}'.format(', '.join(abis)))
+  subprocess.check_call(build_cmd)
+
+  print('Finished building Vulkan validation layers')
 
 
 if __name__ == '__main__':
-    main()
+  main()
