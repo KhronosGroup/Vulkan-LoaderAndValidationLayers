5426d5e8bab894b409459038c808506adc515266
Chris Forbes <chrisforbes@google.com>
2017-04-04 17:57:58 -0700

    tests: Add test for a specialization-induced array size mismatch
    These shaders are fine without the specialization -- but with it
    applied, we should get a type mismatch on the array sizes.

--- tests/layer_validation_tests.cpp
+++ tests/layer_validation_tests.cpp
@@ -14745,6 +14745,59 @@ TEST_F(VkLayerTest, CreatePipelineVsFsArraySizeDefaultSpecialization) {
     m_errorMonitor->VerifyFound();
 }
 
+TEST_F(VkLayerTest, CreatePipelineVsFsArraySizeSpecialization) {
+    m_errorMonitor->SetDesiredFailureMsg(VK_DEBUG_REPORT_ERROR_BIT_EXT, "Type mismatch");
+
+    // Trigger a type mismatch when the VS output array size is specialized. If
+    // specializations are not applied, then these interfaces match fine.
+
+    ASSERT_NO_FATAL_FAILURE(Init());
+    ASSERT_NO_FATAL_FAILURE(InitRenderTarget());
+
+    char const *vsSource =
+        "#version 450\n"
+        "out gl_PerVertex {\n"
+        "    vec4 gl_Position;\n"
+        "};\n"
+        "layout(constant_id=0) const uint size = 1;\n"
+        "layout(location=0) out float xs[size];\n"
+        "void main(){\n"
+        "   gl_Position = vec4(0);\n"
+        "   for (int i = 0; i < size; i++) { xs[i] = 42.0; }\n"
+        "}\n";
+    char const *fsSource =
+        "#version 450\n"
+        "\n"
+        "layout(location=0) in float xs[1];\n"
+        "layout(location=0) out vec4 color;\n"
+        "void main(){\n"
+        "   color = vec4(xs[0]);\n"
+        "}\n";
+
+    VkShaderObj vs(m_device, vsSource, VK_SHADER_STAGE_VERTEX_BIT, this);
+    VkShaderObj fs(m_device, fsSource, VK_SHADER_STAGE_FRAGMENT_BIT, this);
+
+    // Specialize the VS
+    uint32_t actual_size = 2;
+    VkSpecializationMapEntry spec_entry = { 0, 0, sizeof(actual_size) };
+    VkSpecializationInfo spec = { 1, &spec_entry, sizeof(actual_size), &actual_size };
+    auto vs_stage = vs.GetStageCreateInfo();
+    vs_stage.pSpecializationInfo = &spec;
+
+    VkPipelineObj pipe(m_device);
+    pipe.AddColorAttachment();
+    pipe.AddShader(vs_stage);
+    pipe.AddShader(&fs);
+
+    VkDescriptorSetObj descriptorSet(m_device);
+    descriptorSet.AppendDummy();
+    descriptorSet.CreateVKDescriptorSet(m_commandBuffer);
+
+    pipe.CreateVKPipeline(descriptorSet.GetPipelineLayout(), renderPass());
+
+    m_errorMonitor->VerifyFound();
+}
+
 TEST_F(VkLayerTest, CreatePipelineDepthStencilRequired) {
     m_errorMonitor->SetDesiredFailureMsg(VK_DEBUG_REPORT_ERROR_BIT_EXT,
                                          "pDepthStencilState is NULL when rasterization is enabled and subpass "
