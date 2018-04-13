f9c0401846d396130764db5c5d071a26fb142c2a
Lenny Komow <lenny@lunarg.com>
2018-04-13 09:45:18 -0600

    layers: Use new settings interface
    Use the new loader/layer interface where the settings file is passed
    into the negotiation function.
    
    Change-Id: I7d98251ab3b4cfe1adc8b27692e625ba74b638b3

--- layers/vk_layer_config.h
+++ layers/vk_layer_config.h
@@ -57,6 +57,7 @@ const std::unordered_map<std::string, VkFlags> report_flags_option_definitions =
     {std::string("error"), VK_DEBUG_REPORT_ERROR_BIT_EXT},
     {std::string("debug"), VK_DEBUG_REPORT_DEBUG_BIT_EXT}};
 
+VK_LAYER_EXPORT void setOptionFilename(char const *settings_file);
 VK_LAYER_EXPORT const char *getLayerOption(const char *_option);
 VK_LAYER_EXPORT FILE *getLayerLogOutput(const char *_option, const char *layerName);
 VK_LAYER_EXPORT VkFlags GetLayerOptionFlags(std::string _option, std::unordered_map<std::string, VkFlags> const &enum_data,
